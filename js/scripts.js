a0d6398d907761de8bfb2105dcd9f03fb7a557e8
Andrew Reynolds <andrew@nearablecorp.com>
2020-03-03 13:16:54 -0800

    creating function for altering full width traffic shart

--- js/scripts.js
+++ js/scripts.js
@@ -84,7 +84,6 @@ trafficUl.addEventListener('click', (e)=>{
         trafficLi[i].className = '';
       }
     }
-
   }
 
 });
@@ -126,6 +125,29 @@ let myChart = new Chart(trafficChart,{
       }
     }
 });
+//////// Function for creating Traffic Chart/////////////////
+function createTrafficChart(chartData, chartOptions){
+  let myChart = new Chart(trafficChart, {
+    type: 'line',
+    data: chartData,
+    options: chartOptions,  
+  })
+}
+
+////////Data Charts for Traffic Full Width///////////////////
+
+const weeklyChart = {
+  labels: ['16-22', '23-29', '30-5', '6-12', '13-19', '20-26', '27-3', '4-10', '11-17', '18-24', '25-31'],
+      datasets: [{
+        data:[750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
+        backgroundColor: 'rgba(116, 119, 191, .3)',
+        borderWidth: 1,
+        pointBackgroundColor: '#fff',
+        pointBorderColor: 'rgb(168, 50, 153)',
+        lineTension: 0.1
+      }],
+};
+
 
 
 ////////////////////////////////////////
